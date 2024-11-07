7bd67bcb8e407232c8d35953c7cb116dabd4e0bb
zwd512 <1363468974@qq.com>
2024-11-07 18:17:12 +0800

    Site updated: 2024-11-07 18:17:11

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2024/10/24/chacao/","2024/10/23/hello-world/","2024/10/24/mastergo/","2024/10/24/在俩一次/","2024/10/24/待送达/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
